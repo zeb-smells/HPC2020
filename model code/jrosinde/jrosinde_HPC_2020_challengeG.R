441b4f8f84fd3d70b4df5f06f406a5ae37f0ff1f
jrosindell <james@rosindell.org>
2020-11-21 17:27:29 +0000

    Create initial materials for start of course

---
+++ model code/jrosinde/jrosinde_HPC_2020_challengeG.R
@@ -0,0 +1,11 @@
+# CMEE 2020 HPC excercises R code challenge G proforma
+
+rm(list=ls()) # nothing written elsewhere should be needed to make this work
+
+# please edit these data to show your information.
+name <- "James Rosindell"
+preferred_name <- "James"
+email <- "j.rosindell@imperial.ac.uk"
+username <- "jrosinde"
+
+# don't worry about comments for this challenge - the number of characters used will be counted starting from here
---
+++ model code/jrosinde/jrosinde_HPC_2020_cluster.R
@@ -0,0 +1,4 @@
+# CMEE 2020 HPC excercises R code HPC run code proforma
+
+rm(list=ls()) # good practice 
+source("jrosinde_HPC_2020_main.R")
