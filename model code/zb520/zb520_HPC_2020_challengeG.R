594b1a57a1e8a65278fc2d91729a8df4cb587beb
zeb-smells <bondzeb@gmail.com>
2020-12-21 18:45:18 +0000

    actual final submission mk2

--- model code/zb520/zb520_HPC_2020_challengeG.R
+++ model code/zb520/zb520_HPC_2020_challengeG.R
@@ -12,11 +12,11 @@ username <- "zb520"
 
 # maybe if u save points and plot as type = "l"
 X=function(s,S,d,l,D){
-    if(l>0.001){ 
+    if(l>0.01){ 
         segments(s,S,e<-s+l*cos(d),E<-S+l*sin(d))
         X(e,E,d+D*4/pi,l*0.38,D)
         X(e,E,d,l*0.87,-1*D)
     }
 }
 plot(NA,,,c<-c(0,8),c)
-X(4,0,5/pi,1,1)
+X(4,0,pi/2,1,1)
